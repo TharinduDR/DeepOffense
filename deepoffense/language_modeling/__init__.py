f84fdf136b1eb093f20b56e9882fb704f6a91589
TharinduDR <rhtdranasinghe@gmail.com>
2020-07-20 16:24:14 +0100

    004: Upgrading

---
+++ deepoffense/language_modeling/__init__.py
@@ -0,0 +1,2 @@
+from algo.neural_nets.models.transformers.language_modeling.language_modeling_model import LanguageModelingModel
+from algo.neural_nets.models.transformers.args.model_args import LanguageModelingArgs
