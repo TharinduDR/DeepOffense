f66786e9c6c611e30c3a647a64409421346c7386
TharinduDR <rhtdranasinghe@gmail.com>
2021-06-28 12:52:56 +0100

    005: Added the citation

---
+++ examples/german/german_deepoffense.py
@@ -0,0 +1,61 @@
+import os
+import shutil
+import time
+import csv
+import numpy as np
+import pandas as pd
+import sklearn
+import torch
+from sklearn.model_selection import train_test_split
+
+from deepoffense.classification import ClassificationModel, MultiLabelClassificationModel
+from deepoffense.language_modeling.language_modeling_model import LanguageModelingModel
+from examples.common.evaluation import macro_f1, weighted_f1
+from examples.common.label_converter import decode, encode
+from examples.german.german_deepoffense_config import LANGUAGE_FINETUNE, TEMP_DIRECTORY, SUBMISSION_FOLDER, \
+    MODEL_TYPE, MODEL_NAME, language_modeling_args, args, SEED, RESULT_FILE
+from examples.common.print_stat import print_information
+
+if not os.path.exists(TEMP_DIRECTORY): os.makedirs(TEMP_DIRECTORY)
+if not os.path.exists(os.path.join(TEMP_DIRECTORY, SUBMISSION_FOLDER)): os.makedirs(
+    os.path.join(TEMP_DIRECTORY, SUBMISSION_FOLDER))
+
+data = pd.read_csv('examples/german/data/GermEval21_Toxic_Train.csv')
+test_data = pd.read_csv('examples/german/data/GermEval21_Toxic_TestData.csv')
+
+training_instances = []
+for index, row in data.iterrows():
+    training_instance = [row['comment_text'], [row['Sub1_Toxic'], row['Sub2_Engaging'], row['Sub3_FactClaiming']]]
+    training_instances.append(training_instance)
+
+testing_ids = []
+testing_texts = []
+for index, row in data.iterrows():
+    testing_ids.append(row["comment_id"])
+    testing_texts.append(row["c_text"])
+
+train = pd.DataFrame(training_instances)
+train.columns = ["text", "labels"]
+
+train_df, eval_df = train_test_split(train, test_size=0.1, random_state=SEED)
+
+model = MultiLabelClassificationModel(MODEL_TYPE, MODEL_NAME, num_labels=3,  args=args, use_cuda=torch.cuda.is_available())
+model.train_model(train_df, eval_df=eval_df, macro_f1=macro_f1, weighted_f1=weighted_f1, accuracy=sklearn.metrics.accuracy_score)
+
+model = MultiLabelClassificationModel(MODEL_TYPE, args["best_model_dir"], num_labels=3,  args=args, use_cuda=torch.cuda.is_available())
+
+# Evaluate the model
+result, model_outputs, wrong_predictions = model.eval_model(eval_df)
+predictions, raw_outputs = model.predict(testing_texts)
+
+prediction_instances = []
+for id, prediction in zip(testing_ids, predictions):
+    prediction_instances.append([id, prediction[0], prediction[1], prediction[2]])
+
+
+predictions = pd.DataFrame(prediction_instances)
+predictions.columns = ["comment_id", "Sub1_Toxic", "Sub2_Engaging", "Sub3_FactClaiming"]
+
+predictions.to_csv(os.path.join(TEMP_DIRECTORY, RESULT_FILE),  header=True, sep=',', index=False, encoding='utf-8')
+
+
