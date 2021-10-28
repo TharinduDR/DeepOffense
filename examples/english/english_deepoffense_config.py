8715006707ac0272cc534b3d6844ef10367400e9
TharinduDR <rhtdranasinghe@gmail.com>
2021-10-28 23:37:36 +0100

    005: Added the citation

--- examples/english/english_deepoffense_config.py
+++ examples/english/english_deepoffense_config.py
@@ -10,7 +10,7 @@ SUBMISSION_FOLDER = "transformers"
 SUBMISSION_FILE = "transformers"
 MODEL_TYPE = "bert"
 MODEL_NAME = "bert-large-cased"
-LANGUAGE_FINETUNE =True
+LANGUAGE_FINETUNE =False
 SEED = 777
 
 # training instances = 7000 > if batch size=8, batches = 875 > evaluate during training steps -> 80 or 175
@@ -22,11 +22,11 @@ args = {
 
     'fp16': False,
     'fp16_opt_level': 'O1',
-    'max_seq_length': 128,  # 128
+    'max_seq_length': 256,  # 128
     'train_batch_size': 8,
     'gradient_accumulation_steps': 1,
     'eval_batch_size': 8,
-    'num_train_epochs': 3,
+    'num_train_epochs': 6,
     'weight_decay': 0,
     'learning_rate': 1e-5,
     'adam_epsilon': 1e-8,
@@ -36,15 +36,15 @@ args = {
     'do_lower_case': False,
     'n_fold': 3,
 
-    'logging_steps': 200,
-    'save_steps': 200,
+    'logging_steps': 300,
+    'save_steps': 300,
     "no_cache": False,
     "no_save": False,
     "save_recent_only": True,
     'save_model_every_epoch': True,
     'evaluate_during_training': True,
     "evaluate_during_training_silent": True,
-    'evaluate_during_training_steps': 200,
+    'evaluate_during_training_steps': 300,
     "evaluate_during_training_verbose": True,
     'use_cached_eval_features': False,
     "save_best_model": True,
